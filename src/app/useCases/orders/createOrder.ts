0080955e7699a1775cdc43c6a2fd19179c023687
Eliseu <124800294+EliseuDLS@users.noreply.github.com>
2023-09-26 22:53:59 -0300

    Update createOrder.ts

--- src/app/useCases/orders/createOrder.ts
+++ src/app/useCases/orders/createOrder.ts
@@ -1,13 +1,23 @@
+//importa as funcionalidades Request e Response
 import { Request, Response } from 'express';
+
+//importa o modelo "Order"
 import { Order } from '../../models/Order';
 
+//função assíncrona chamada createOrder para criar um pedido
 export async function createOrder(req: Request, res: Response) {
 	try {
+
+		//obtém os dados da solicitação
 		const {table, products} = req.body;
 
+		//cria um novo pedido no banco de dados
 		const order = await Order.create({ table, products });
+
+		//se a criação do pedido for bem sucedida, a função responde à solicitação e retorna o novo pedido criado em formato JSON
 		res.status(201).json(order);
 	} catch (error) {
+		//se ocorrer algum erro durante o processo, a função responde indicando que houve um problema
 		console.log(error);
 		res.sendStatus(500);
 	}
