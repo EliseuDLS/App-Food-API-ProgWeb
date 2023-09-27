5cbe9b699dee8aa35c20c93760e3ff77363f551e
Eliseu <124800294+EliseuDLS@users.noreply.github.com>
2023-09-26 22:51:28 -0300

    Update Product.ts

--- src/app/models/Product.ts
+++ src/app/models/Product.ts
@@ -1,35 +1,46 @@
+//importa as bibliotecas
 import {model, Schema } from 'mongoose';
 
+//classe 'Product' que representa os produtos
 export const Product = model('Product', new Schema({
+
+	//nome do produto e é uma string obrigatória
 	name: {
 		type: String,
 		required: true,
 	},
+	//descrição do produto e é uma string obrigatória
 	description: {
 		type: String,
 		required: true,
 	},
+	//caminho para a imagem do produto e é uma string obrigatória
 	imagePath: {
 		type: String,
 		required: true,
 	},
+	//preço do produto e é um número obrigatório
 	price: {
 		type: Number,
 		required: true,
 	},
+	//lista de ingredientes em um produto
 	ingredients: {
 		required: true,
 		type:[{
+			//nome do ingrediente
 			name: {
 				type: String,
 				required: true,
 			},
+			//ícone do ingrediente
 			icon: {
 				type: String,
 				required: true,
 			},
 		}],
 	},
+	//referência a uma categoria
 	category: {
 		type: Schema.Types.ObjectId,
 		required: true,
