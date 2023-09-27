580cb7486e98ecf43506d8bebecc68026bcb1b13
Eliseu <124800294+EliseuDLS@users.noreply.github.com>
2023-09-26 22:52:48 -0300

    Update listProductsByCategory.ts

--- src/app/useCases/categories/listProductsByCategory.ts
+++ src/app/useCases/categories/listProductsByCategory.ts
@@ -1,14 +1,22 @@
+//importa as funcionalidades Request e Response
 import { Request, Response } from 'express';
+
+//importa o modelo "Product"
 import { Product } from '../../models/Product';
 
+//função assíncrona chamada listProductsByCategory para listagem de produtos 
 export async function listProductsByCategory(req: Request, res: Response) {
 	try {
+		//ela obtém o parâmetro categoryId
 		const {categoryId} = req.params;
 
+		//consulta todos os produtos no banco de dados que pertencem à categoria especificada pelo categoryId
 		const products = await Product.find().where('category').equals(categoryId);
 
+		//se a consulta for bem sucedida, a função responde com a lista de produtos em formato JSON 
 		res.json(products);
 	} catch (error) {
+		//se ocorrer algum erro durante o processo, a função responde indicando que houve um problema
 		console.log(error);
 		res.sendStatus(500);
 	}
