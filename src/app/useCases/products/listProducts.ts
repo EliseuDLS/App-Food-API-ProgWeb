9c7201d5b1a724afe15a658796c7fa1b245a158a
Eliseu <124800294+EliseuDLS@users.noreply.github.com>
2023-09-26 23:00:26 -0300

    Update listProducts.ts

--- src/app/useCases/products/listProducts.ts
+++ src/app/useCases/products/listProducts.ts
@@ -1,12 +1,19 @@
+//importa as funcionalidades Request e Response
 import { Request, Response } from 'express';
+
+//importa o modelo "Order"
 import { Product } from '../../models/Product';
 
+//função assíncrona chamada listProducts para listar os produtos
 export async function listProducts(req: Request, res: Response) {
 	try {
+		//consulta todos os produtos no banco de dados
 		const products = await Product.find();
 
+		//se a consulta for bem sucedida, a função responde com a lista de produtos em formato JSON 
 		res.json(products);
 	} catch (error) {
+		//se ocorrer algum erro durante o processo, a função responde indicando que houve um problema
 		console.log(error);
 		res.sendStatus(500);
 	}
