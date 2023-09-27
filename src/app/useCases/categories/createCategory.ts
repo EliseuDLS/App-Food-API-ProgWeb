941c5335654da6eac532224e04d4b36b8f4b1d02
Eliseu <124800294+EliseuDLS@users.noreply.github.com>
2023-09-26 22:52:06 -0300

    Update createCategory.ts

--- src/app/useCases/categories/createCategory.ts
+++ src/app/useCases/categories/createCategory.ts
@@ -1,13 +1,22 @@
+//importa as funcionalidades Request e Response
 import { Request, Response } from 'express';
+
+//importa o modelo "Category"
 import { Category } from '../../models/Category';
 
+//função assíncrona chamada createCategory para criação de uma nova categoria
 export async function createCategory(req: Request, res: Response) {
 	try {
-		//res.send('Ok post category');
+		//obter os valores icon e name
 		const {icon, name} = req.body;
+
+		//cria uma nova categoria
 		const category = await Category.create({icon, name});
+		
+		//se a criação for bem sucedida, retorna a categoria criada como JSON
 		res.status(201).json(category);
 	} catch (error) {
+		//se ocorrer algum erro durante o processo, a função responde indicando que houve um problema
 		console.log(error);
 		res.sendStatus(500);
 	}
