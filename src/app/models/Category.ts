8eb4b9b2af01156a34e2f50cfde8095f3088bc51
Eliseu <124800294+EliseuDLS@users.noreply.github.com>
2023-09-26 22:50:29 -0300

    Update Category.ts

--- src/app/models/Category.ts
+++ src/app/models/Category.ts
@@ -1,12 +1,15 @@
 //importa as bibliotecas
 import {model, Schema } from 'mongoose';
 
-//classe 'Category' com dois campos 'name' e 'icon'
+//classe 'Category' que armazena informações sobre categorias, que deverão ter um nome e um ícone definidos
 export const Category = model('Category', new Schema({
+
+	//nome da categoria e é do tipo String
 	name: {
 		type: String,
 		required: true,
 	},
+	//ícone associado à categoria e também é do tipo String
 	icon: {
 		type: String,
 		required: true,
