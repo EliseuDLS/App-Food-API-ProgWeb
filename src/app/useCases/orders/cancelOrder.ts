25f393d83965456493a829bb4747482c5a3368bd
Eliseu <124800294+EliseuDLS@users.noreply.github.com>
2023-09-26 22:53:29 -0300

    Update cancelOrder.ts

--- src/app/useCases/orders/cancelOrder.ts
+++ src/app/useCases/orders/cancelOrder.ts
@@ -1,13 +1,22 @@
+//importa as funcionalidades Request e Response
 import { Request, Response } from 'express';
+
+//importa o modelo "Order"
 import { Order } from '../../models/Order';
 
+//função assíncrona chamada cancelOrder para cancelamento de um pedido 
 export async function cancelOrder(req: Request, res: Response) {
 	try {
+		//obtém o parâmetro orderId
 		const { orderId } = req.params;
-		await Order.findByIdAndDelete(orderId);
-		res.sendStatus(204);
 
+		//procura e exclui o pedido no banco de dados com base no orderId 
+		await Order.findByIdAndDelete(orderId);
+
+		//se a exclusão for bem-sucedida, a função responde à solicitação HTTP
+		res.sendStatus(204);
 	} catch (error) {
+		//se ocorrer algum erro durante o processo, a função responde indicando que houve um problema
 		console.log(error);
 		res.sendStatus(500);
 	}
